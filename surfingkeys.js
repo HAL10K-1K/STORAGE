c22c3a0a7fce7af736e7cb5670b37272f22e22eb
KY <ebfkspcqkd@hanmail.net>
2018-06-02 04:30:21 +0900

    add surfingkeys

---
+++ surfingkeys.js
@@ -0,0 +1,54 @@
+
+// #region keymaps 
+map('gt', 'T');
+
+// #endregion
+
+// #region emoji
+settings.startToShowEmoji = 0;
+iunmap(':');
+
+// #endregion
+
+// #region theme
+settings.theme = `
+.sk_theme {
+    background: #000;
+    color: #fff;
+}
+.sk_theme tbody {
+    color: #fff;
+}
+.sk_theme input {
+    color: #d9dce0;
+}
+.sk_theme .url {
+    color: #2173c5;
+}
+.sk_theme .annotation {
+    color: #38f;
+}
+.sk_theme .omnibar_highlight {
+    color: #fbd60a;
+}
+.sk_theme ul>li:nth-child(odd) {
+    background: #1e211d;
+}
+.sk_theme ul>li.focused {
+    background: #4ec10d;
+}`;
+
+// #endregion
+
+// #region searchalias
+addSearchAliasX('ex', 'Chrome Extension', 'https://chrome.google.com/webstore/search/');
+addSearchAliasX('nm', 'Namu Wiki', 'https://namu.wiki/go/');
+addSearchAliasX('mr', 'Maven Repository', 'https://mavenrepository.com/search?q=');
+addSearchAliasX('tz', 'Torrentz2', 'https://torrentz2.eu/search?f=');
+addSearchAliasX('hr', 'HDReactor', 'http://hdreactor.su/?do=search&subaction=search&x=0&y=0&title_only=3&fullsearch=0&story=');
+addSearchAliasX('rb', 'RARBG', 'http://rarbg.is/torrents.php?search=');
+addSearchAliasX('du', 'DuckDuckGo', 'https://duckduckgo.com/?q=');
+
+// #endregion
+
+
