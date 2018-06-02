88ef9983e2020e946282dfd4c54ebb71f31273ce
KY <voidkknd17@gmail.com>
2018-06-03 02:38:41 +0900

    change alias

--- surfingkeys.js
+++ surfingkeys.js
@@ -44,10 +44,9 @@ settings.theme = `
 addSearchAliasX('ex', 'Chrome Extension', 'https://chrome.google.com/webstore/search/');
 addSearchAliasX('nm', 'Namu Wiki', 'https://namu.wiki/go/');
 addSearchAliasX('mr', 'Maven Repository', 'https://mavenrepository.com/search?q=');
-addSearchAliasX('tz', 'Torrentz2', 'https://torrentz2.eu/search?f=');
-addSearchAliasX('hr', 'HDReactor', 'http://hdreactor.su/?do=search&subaction=search&x=0&y=0&title_only=3&fullsearch=0&story=');
-addSearchAliasX('rb', 'RARBG', 'http://rarbg.is/torrents.php?search=');
-addSearchAliasX('du', 'DuckDuckGo', 'https://duckduckgo.com/?q=');
+addSearchAliasX('tt', 'Torrentz2', 'https://torrentz2.eu/search?f=');
+addSearchAliasX('tr', 'HDReactor', 'http://hdreactor.su/?do=search&subaction=search&x=0&y=0&title_only=3&fullsearch=0&story=');
+addSearchAliasX('tb', 'RARBG', 'http://rarbg.is/torrents.php?search=');
 
 // #endregion
 
