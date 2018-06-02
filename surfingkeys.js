57b7a46d1ed7af421ca8a8341db296abf942f198
HAL10K-1K <ebfkspcqkd@hanmail.net>
2018-06-03 03:08:53 +0900

    organized source

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,14 +1,13 @@
-
 // #region keymaps 
 map('gt', 'T');
-
 // #endregion
+
 
 // #region emoji
 settings.startToShowEmoji = 0;
 iunmap(':');
-
 // #endregion
+
 
 // #region theme
 settings.theme = `
@@ -37,8 +36,8 @@ settings.theme = `
 .sk_theme ul>li.focused {
     background: #4ec10d;
 }`;
-
 // #endregion
+
 
 // #region searchalias
 addSearchAliasX('ex', 'Chrome Extension', 'https://chrome.google.com/webstore/search/');
@@ -47,7 +46,4 @@ addSearchAliasX('mr', 'Maven Repository', 'https://mavenrepository.com/search?q=
 addSearchAliasX('tt', 'Torrentz2', 'https://torrentz2.eu/search?f=');
 addSearchAliasX('tr', 'HDReactor', 'http://hdreactor.su/?do=search&subaction=search&x=0&y=0&title_only=3&fullsearch=0&story=');
 addSearchAliasX('tb', 'RARBG', 'http://rarbg.is/torrents.php?search=');
-
 // #endregion
-
-
