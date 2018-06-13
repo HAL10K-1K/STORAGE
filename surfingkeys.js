384d95935ce5ca13b81013d4c8f488070da8cfa6
HAL10K-1K <ebfkspcqkd@hanmail.net>
2018-06-13 18:37:15 +0900

    theme part removed

--- surfingkeys.js
+++ surfingkeys.js
@@ -9,42 +9,6 @@ iunmap(':');
 // #endregion
 
 
-// #region theme
-settings.theme = `
-.sk_theme {
-    background: #000;
-    color: #fff;
-}
-.sk_theme tbody {
-    color: #fff;
-}
-.sk_theme input {
-    color: #d9dce0;
-}
-.sk_theme .url {
-    color: #2173c5;
-}
-.sk_theme .annotation {
-    color: #38f;
-}
-.sk_theme .omnibar_highlight {
-    color: #fbd60a;
-}
-.sk_theme ul>li {
-    background: #29447594;
-}
-.sk_theme #sk_omnibarSearchResult>ul>li:nth-child(odd) {
-    background: #000;
-}
-.sk_theme ul>li:nth-child(odd) {
-    background: #000;
-}
-.sk_theme ul>li.focused {
-    background: #4ec10d;
-}`;
-// #endregion
-
-
 // #region searchalias
 addSearchAliasX('ex', 'Chrome Extension', 'https://chrome.google.com/webstore/search/');
 addSearchAliasX('nm', 'Namu Wiki', 'https://namu.wiki/go/');
